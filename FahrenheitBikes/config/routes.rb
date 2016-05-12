a56ed2304024d6bbb69a1455b93c3e7b6602f053
Hdtran89 <hdtran89@yahoo.com>
2016-05-12 00:21:22 -0700

    fix bootstrap

--- FahrenheitBikes/config/routes.rb
+++ FahrenheitBikes/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root "fahrenheit#index"
+  	 get "update" => "fahrenheit#update"
+  	 get "shop" => "fahrenheit#shop"
+  	 get "testride" => "fahrenheit#testride"
+  	 get "shop" => "fahrenheit#shop"
+  	 get "about" => "fahrenheit#about"
+  	 get "contact" => "fahrenheit#contact"
 end
